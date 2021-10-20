2c9297bce4961d7939d7e8b580f97f86bf6df77b
Adhikram <adhikramm@gmail.com>
2021-10-20 21:44:42 +0530

    🔥 updates

--- Q01_sort012/sort012.cpp
+++ Q01_sort012/sort012.cpp
@@ -2,18 +2,29 @@
 using namespace std;
 int main()
 {
-  int t;
-  cin>>t;
-  vector<int> v(t, 0);
-
-  for (int i = 0; i < t; i++)
+  ifstream infile;
+  ofstream outfile;
+  string s = "/media/adhikram/Arka/Devsnest_DSA/Q01_sort012/Q01_sort012_";
+  for (int i = 1; i < 11; i++)
   {
-    cin >> v[i];
-  }
+    string p = s + to_string(i);
+    infile.open(p+"_hidden_in.txt");
+    outfile.open(p+"_hidden_out.txt",ios::out | ios::trunc);
+    int t;
+    infile >> t;
+    vector<int> v(t, 0);
 
-  sort(v.begin(), v.end());
-  for (int i = 0; i < t; i++)
-  {
-    cout << v[i] << " ";
+    for (int i = 0; i < t; i++)
+    {
+      infile >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+    for (int i = 0; i < t; i++)
+    {
+      outfile << v[i] << " ";
+    }
+    infile.close();
+    outfile.close();
   }
 }
