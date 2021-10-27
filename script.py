0f092b86499d3dcd6e05b53227bc1efeb05cc112
Adhikram <adhikramm@gmail.com>
2021-10-28 03:49:50 +0530

    🚑

--- script.py
+++ script.py
@@ -27,7 +27,7 @@
 # path 
 import random
 
-name="Q18_Combinations_"
+name="Q39_Permutation_Sequence_"
 total=1
 # N=[2,5,10,50,100,500,1000,5000,10000,50000,100000]
 # N=[1,5,10,20,30,40,60,70,80,90,100]
@@ -36,20 +36,21 @@ total=1
 # N=[1,50,100,250,500,700,750,800,900,950,1000]
 # N=[15,50,100,150,200,250,500,1000,5000,6000,10000]
 N=[1,3,5,7,9,11,13,15,17,19,21]
+t=[1,2,3,4,5,6,7,8,9]
 while(total<11):
   f=open(name+str(total)+'_hidden_in.txt', 'w')
   
   number = random.randint(N[total-1], N[total])
-  number1 = random.randint(N[total-1], number)
+  # number1 = random.randint(N[total-1], number)
   
   
-  f.write(str(number))
+  f.write(str(total))
   f.writelines(" ")
-  f.write(str(number1))
+  f.write(str(number))
   f.writelines("\n")
   # s=""
   # for i in range(0,number):
-  #   s += str(random.randint(0,N[total-1]*10))+" "
+  #   s += str(random.randint(N[total-1],N[total]))+" "
 
   # f.write(s[:-1])
   # f.writelines("\n")
