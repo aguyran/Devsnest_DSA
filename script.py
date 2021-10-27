1a1fa87b5929a29d7023515d536a93dbdaf39745
Adhikram <adhikramm@gmail.com>
2021-10-28 00:21:08 +0530

    🚨

--- script.py
+++ script.py
@@ -27,30 +27,32 @@
 # path 
 import random
 
-name="Q15_Maximize sum_"
+name="Q18_Combinations_"
 total=1
-N=[2,5,10,50,100,500,1000,5000,10000,50000,100000]
+# N=[2,5,10,50,100,500,1000,5000,10000,50000,100000]
 # N=[1,5,10,20,30,40,60,70,80,90,100]
-# N=[2,5,10,15,20,25,30,35,40,45,50]
+# N=[1,5,10,15,20,25,30,35,40,45,50]
 # N=[1,50,100,150,200,250,300,350,400,450,500]
 # N=[1,50,100,250,500,700,750,800,900,950,1000]
 # N=[15,50,100,150,200,250,500,1000,5000,6000,10000]
+N=[1,3,5,7,9,11,13,15,17,19,21]
 while(total<11):
-  f=open(name+str(total)+"_hidden_in.txt", 'w')
+  f=open(name+str(total)+'_hidden_in.txt', 'w')
   
   number = random.randint(N[total-1], N[total])
-  # number1 = random.randint(N[total-1],  N[total])
+  number1 = random.randint(N[total-1], number)
+  
   
   f.write(str(number))
-  # f.writelines(" ")
-  # f.write(str(number//2))
+  f.writelines(" ")
+  f.write(str(number1))
   f.writelines("\n")
-  s=""
-  for i in range(0,number):
-    s += str(random.randint(N[total-1],N[total]))+" "
+  # s=""
+  # for i in range(0,number):
+  #   s += str(random.randint(0,N[total-1]*10))+" "
 
-  f.write(s[:-1])
-  f.writelines("\n")
+  # f.write(s[:-1])
+  # f.writelines("\n")
   # s=""
   # for i in range(0,number1):
   #   s += str(random.randint(0,1000000))+" "
@@ -59,4 +61,3 @@ while(total<11):
   # f.writelines("\n")
   f.close()
   total+=1
-
--- testcases.py
+++ testcases.py
@@ -1,30 +1,31 @@
 import random
 
-name="/media/adhikram/Arka/Devsnest_DSA/Q32_Find_first_and_last_positions_of_an_element_in_a_sorted_array/Find_first_and_last_positions_of_an_element_in_a_sorted_array_hidden_sample_"
+name="Q18_Combinations_"
 total=1
-N=[2,5,10,50,100,500,1000,5000,10000,50000,100000]
+# N=[2,5,10,50,100,500,1000,5000,10000,50000,100000]
 # N=[1,5,10,20,30,40,60,70,80,90,100]
 # N=[1,5,10,15,20,25,30,35,40,45,50]
 # N=[1,50,100,150,200,250,300,350,400,450,500]
 # N=[1,50,100,250,500,700,750,800,900,950,1000]
 # N=[15,50,100,150,200,250,500,1000,5000,6000,10000]
+N=[1,3,5,7,9,11,13,15,17,19,21]
 while(total<11):
   f=open(name+str(total)+'.txt', 'w')
   
   number = random.randint(N[total-1], N[total])
   number1 = random.randint(N[total-1], number-1)
-  total+=1
+  
   
   f.write(str(number))
   f.writelines(" ")
   f.write(str(number1))
   f.writelines("\n")
-  s=""
-  for i in range(0,number):
-    s += str(random.randint(0,N[total-1]*10))+" "
+  # s=""
+  # for i in range(0,number):
+  #   s += str(random.randint(0,N[total-1]*10))+" "
 
-  f.write(s[:-1])
-  f.writelines("\n")
+  # f.write(s[:-1])
+  # f.writelines("\n")
   # s=""
   # for i in range(0,number1):
   #   s += str(random.randint(0,1000000))+" "
@@ -32,3 +33,4 @@ while(total<11):
   # f.write(s[:-1])
   # f.writelines("\n")
   f.close()
+  total+=1
