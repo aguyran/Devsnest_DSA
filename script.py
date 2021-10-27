90d94bf205bb73b7715b8c6b1d359e9a997ca539
Adhikram <adhikramm@gmail.com>
2021-10-27 22:23:13 +0530

    ✨

--- script.py
+++ script.py
@@ -27,37 +27,45 @@
 # path 
 import random
 
-name="Q28_Circular_Queue_"
+name="Q48_LCS_of_three_strings_"
 total=1
-N=[2,5,10,50,100,500,1000,5000,10000,50000,100000]
+# N=[2,5,10,50,100,500,1000,5000,10000,50000,100000]
 # N=[1,5,10,20,30,40,60,70,80,90,100]
-# N=[1,5,10,15,20,25,30,35,40,45,50]
+N=[2,5,10,15,20,25,30,35,40,45,50]
 # N=[1,50,100,150,200,250,300,350,400,450,500]
 # N=[1,50,100,250,500,700,750,800,900,950,1000]
 # N=[15,50,100,150,200,250,500,1000,5000,6000,10000]
+t=["A","B","C","D","E","F"]
 while(total<11):
   f=open(name+str(total)+"_hidden_in.txt", 'w')
   
   number = random.randint(N[total-1], N[total])
-#   number1 = random.randint(N[total-1], number-1)
+  number1 = random.randint(N[total-1], N[total])
+  number2 = random.randint(N[total-1], N[total])
   
   
   f.write(str(number))
-#   f.writelines(" ")
-#   f.write(str(number1))
+  f.writelines(" ")
+  f.write(str(number1))
   f.writelines("\n")
-#   s=""
-#   for i in range(0,number):
-#     s += str(random.randint(1,N[total-1]))+" "
+  s=""
+  for i in range(0,number):
+    s += str(random.choice(t))
 
-#   f.write(s[:-1])
-#   f.writelines("\n")
-  # s=""
-  # for i in range(0,number1):
-  #   s += str(random.randint(0,1000000))+" "
+  f.write(s[:-1])
+  f.writelines("\n")
+  s=""
+  for i in range(0,number1):
+    s += str(random.choice(t))
 
-  # f.write(s[:-1])
-  # f.writelines("\n")
+  f.write(s[:-1])
+  f.writelines("\n")
+  s=""
+  for i in range(0,number2):
+    s += str(random.choice(t))
+
+  f.write(s[:-1])
+  f.writelines("\n")
   f.close()
   total+=1
 
